2d698e7783d3ccb734892bfeb028c42de6cdf579
arturoguerrab <arturoguerrab12@gmail.com>
2023-09-18 14:28:17 -0300

    whatsapp button

--- src/components/Layout/LayoutContainer.jsx
+++ src/components/Layout/LayoutContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeaderContainer from './Header/HeaderContainer'
 import FooterContainer from './Footer/FooterContainer'
+import { NavLink } from 'react-router-dom'
 
 
 const Layout = ({children}) => {
@@ -8,6 +9,9 @@ const Layout = ({children}) => {
     <div>
         <HeaderContainer/>
             {children}
+          <NavLink to={'https://wa.me/14078641606'} target='_blank'>
+            <button className='fixed z-30 bottom-[2.8rem] right-[1.3rem] h-[45px] w-[45px] bg-transparent'><img className='fixed z-30 bottom-7 right-1 bg h-[80px] w-[80px]' src="https://static.vecteezy.com/system/resources/previews/021/460/383/original/whatsapp-logo-free-png.png" alt="" /></button>
+          </NavLink>
         <FooterContainer/>
     </div>
   )
