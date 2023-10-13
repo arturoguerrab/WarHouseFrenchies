113a73afffe56db60a24f9042ed0f567795ef32b
Arturo Guerra <arturoguerrab12@gmail.com>
2023-10-12 21:42:52 -0300

    prueba

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className={'col-span-12 lg:col-span-4 lg:col-start-2 row-span-3 row-start-1 landscape:row-start-2 lg:row-start-2 lg:mt-[-70px] text-center flex flex-col justify-center items-center'}>
             <h1 className='text-secondary font-bold text-6xl'>WarHouse</h1>
             <h2 className='pl-20 font-bold text-6xl'>Frenchies</h2>
-            <p className='font-semibold w-3/4 py-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi!</p>
+            <p className='font-semibold w-3/4 py-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi!hit</p>
             <Btn style={'w-[180px] h-[40px] self-center'} to={'/puppies'} text={'Ver Mas'}/>
         </div>
 
